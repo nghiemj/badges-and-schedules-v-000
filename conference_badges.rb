c1b77e46963e551f86ff910ac05710988aa6cd2d
James Nghiem <nghiem.j@hotmail.com>
2018-09-26 02:47:45 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+require "pry"
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+ badge_messages = []
+ names.each do |i|
+badge_messages << "Hello, my name is #{i}."
+end
+return badge_messages
+end
+
+def assign_rooms(rooms)
+  room_assignment = []
+  rooms.each_with_index do |name, room_number|
+  room_assignment << "Hello, #{name}! You'll be assigned to room #{room_number+=1}!"
+end
+return room_assignment
+end
+
+def printer(attendees)
+assign_rooms(attendees).each do |attendees|
+binding.pry
+ end
+ end
+ 
